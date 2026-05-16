3e214666ec0b89209ff065e19b3949f322fa6be9
sonnk.010 <sonnk.010@gmail.com>
2026-05-16 15:48:19 +0700

    add search in directory

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,31 @@ vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
 vim.keymap.set("i", "<C-E>", "<C-X><C-E>")
 vim.keymap.set("i", "<C-Y>", "<C-X><C-Y>")
 vim.keymap.set("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+
+local function get_explorer_dir()
+  local explorer = Snacks.picker.get({ source = "explorer" })[1]
+
+  if not explorer then
+    return vim.fn.getcwd()
+  end
+
+  return explorer:dir()
+end
+
+vim.keymap.set("n", "<leader>fd", function()
+  Snacks.picker.files({
+    cwd = get_explorer_dir(),
+    hidden = true,
+  })
+end, {
+  desc = "Find files in selected directory",
+})
+
+vim.keymap.set("n", "<leader>fs", function()
+  Snacks.picker.grep({
+    cwd = get_explorer_dir(),
+    hidden = true,
+  })
+end, {
+  desc = "Grep in selected directory",
+})
